d58b500d94bc879eb5eff4494c8fba426a2ffac6
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-02-15 00:57:59 +0800

    Contact Tracing Information has been displayed on the QR Code Image.

--- villaQRCGen.py
+++ villaQRCGen.py
@@ -16,6 +16,7 @@
 
 # QRCODE Library has been used due to the form request instructed from the Assignment on PLD.
 import qrcode
+from PIL import Image
 
 """
 LAYOUT AND DIMENSIONS FOR VILLARIZA QR CODE PNG FILE.
@@ -24,10 +25,10 @@ encode = qrcode.QRCode(
     # The general size of the QR Code.
     box_size = 10,
     # Alignment Pattern of the BCH Code.
-    version = 5,
+    version = 1,
     # Distance from the QR Dataset Symbol Pattern
     border = 2,
-    # Reed-Solomon Error Correction (H = High )= 30% of data bytes can be stored.
+    # Reed-Solomon Error Correction (H = High ) = 30% of data bytes can be stored.
     error_correction = qrcode.constants.ERROR_CORRECT_H 
 )
 
@@ -35,8 +36,45 @@ encode = qrcode.QRCode(
 QR CODE GENERATOR LAYOUT
 """
 # Data has been stored on the QR Code in an Alphanumeric Pattern.
-details = 'Test, Hello World!'; encode.add_data(details)
+details = """THE COVID-19 HEALTH MONITOR - CONTACT TRACING FORM
+MANILA, PHILIPPINES
+
+Personal Information
+    Full Name       : Fritz Cedrick V. Villariza
+    Sex / Gender    : Male (M)
+    Age             : 22
+    Birthdate       : January 1, 2000
+    Home Address    : 310 San Rafael Street, San Miguel 1005, Manila City
+    Phone Number    : 0912-345-6789
+    Electronic Mail : fcv1995@yahoo.com
+
+Medical Information
+    Weight (in kgs) : 77
+    Height (in cms) : 6'0
+    Comorbidity     : Bronchial Asthma
+    Allergy         : Nicotine
+    Condition (Now) : None
+
+Vaccination Report
+    Fully Vaccinated? (Maximum of Two (2) Dose) If YES, select the brand you received and include the date and place; if NO, please specify.
+        > June 6, 2021     : Sinovac-CoronaVac (Manila Sports Complex)
+        > July 6, 2021     : Sinovac-CoronaVac (Manila Sports Complex)
+
+    Already had a BOOSTER Vaccine? If YES, select the brand you received and include the date and place; if NO, please specify.
+        > January 15, 2022 : BioNTech, Pfizer  (Manila Sports Complex)"""
+
+encode.add_data(details)
 # QR Code Presets and Illustrative Design for the making of PNG File.
-encode.make(fit = True); icon = encode.make_image(back_color = 'black', fill_color = 'cyan')
+encode.make(fit = True); icon = encode.make_image(back_color = 'white', fill_color = 'black').convert('RGB')
+# The QR code will be saved from its Python Settings upon the dictation of the naming file.
+
+"""
+LOGO IMAGE HEADER FOR THE QR CODE PNG FILE (DESIGN PURPOSES)
+"""
+logo_display = Image.open('MARIN KITAGAWA.png') # PNG is imported through Image module.
+logo_display.thumbnail((180, 180)) # Aspect Ratio of the imported PNG File.
+logo_pos = ((icon.size[0] - logo_display.size[0]) // 2, (icon.size[1] - logo_display.size[1]) // 2) # Image Size Presets has been customized.
+icon.paste(logo_display, logo_pos) # PNG is displayed through the "icon" global variable.
+
 # The QR code will be saved from its Python Settings upon the dictation of the naming file.
 icon.save('Villariza QR Code.png')
